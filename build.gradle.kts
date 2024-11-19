fa78e39f379d9cd4929c36200a50a52d9f2d6490
Sudeep Raj <141497724+sudeepraj242@users.noreply.github.com>
2024-11-19 18:09:42 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,70 @@
 plugins {
-    id("com.android.application") version "7.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.6.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+android {
+    compileSdk = 33
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("signing/release.jks")
+            storePassword = "epicdebug"
+            keyAlias = "epicdebug"
+            keyPassword = "epicdebug"
+        }
+
+        getByName("debug") {
+            storeFile = file("signing/debug.jks")
+            storePassword = "epicdebug"
+            keyAlias = "epicdebug"
+            keyPassword = "epicdebug"
+        }
+    }
+
+    defaultConfig {
+        applicationId = "kolmachikhin.alexander.epicto_dolist"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 84
+        versionName = "2.0.6"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+        }
+
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("androidx.work:work-runtime:2.7.1")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.room:room-runtime:2.4.2")
+    kapt("androidx.room:room-compiler:2.4.2")
+    implementation("com.daimajia.swipelayout:library:1.2.0@aar")
+    implementation("com.github.devsideal:VectorChildFinder:1.0.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.github.bumptech.glide:glide:4.13.2")
+    kapt("com.github.bumptech.glide:compiler:4.13.2")
+    implementation("com.google.code.gson:gson:2.9.0")
 }
